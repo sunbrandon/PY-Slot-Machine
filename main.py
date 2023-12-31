b3811e8c6168ec54d1bb8479131e7ef29c4c2ced
sunbrandon <sunabrandon@gmail.com>
2023-12-31 15:35:48 -0800

    Created function to get bets from user

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 MAX_LINES = 3                                                           # global const variable
-
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():                                                          # def = function
     while True:                                                         # while loop checks if user input is valid, if not ask again
@@ -29,9 +30,34 @@ def get_number_of_lines():
 
     return lines
 
+def get_bet():
+    while True:                       
+        amount = input("What would you like to bet on each line? $") 
+        if amount.isdigit():    
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")                  # min and max bet are converted to strings by f string in py to be used in output
+        else:
+            print("Please enter a number.")
+
+    return amount
+
 def main():                                                             # main function where program is called
     balance = deposit()                                                 # runs the deposit function and puts the value into variable "balance"
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        
+        if total_bet > balance:
+            print(
+                f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+    
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
 
 main()
