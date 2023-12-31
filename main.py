675a388d05984f0b476db27a08fca614b9644316
sunbrandon <sunabrandon@gmail.com>
2023-12-31 15:15:04 -0800

    Created function to get num of lines to bet on and main func

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3                                                           # global const variable
+
 
 def deposit():                                                          # def = function
     while True:                                                         # while loop checks if user input is valid, if not ask again
@@ -12,3 +14,24 @@ def deposit():                                                          # def = 
             print("Please enter a number.")
 
     return amount
+
+def get_number_of_lines():
+    while True:                                                     
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")  # prompt user to enter max num of lines and convert MAX_LINES into a string to be used in output msg
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:                                                    # checks if lines is between max and min number
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+def main():                                                             # main function where program is called
+    balance = deposit()                                                 # runs the deposit function and puts the value into variable "balance"
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
